3c4d997923a1ad3601bdddf6aab3b5fb8bbc08da
Chia-chi Yeh <chiachi@android.com>
2010-08-24 15:57:07 +0800

    Speex: build the echo canceller for our RTP stack.
    Change-Id: I01e3700b49e7cd1cdf65aca05cc376f9cb0bf6a4

--- Android.mk
+++ Android.mk
@@ -1,12 +1,27 @@
-LOCAL_PATH:= $(call my-dir)
+#
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_SRC_FILES:= libspeex/fftwrap.c \
-	libspeex/filterbank.c \
+LOCAL_SRC_FILES := \
 	libspeex/mdf.c \
-	libspeex/preprocess.c \
+	libspeex/fftwrap.c \
 	libspeex/smallft.c
 
 LOCAL_MODULE:= libspeex

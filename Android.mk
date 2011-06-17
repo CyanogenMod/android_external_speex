a65d1071006675bbb6cf7a8c2a4bd3c8b961a70c
Eric Laurent <elaurent@google.com>
2011-06-16 21:00:46 -0700

    Created speex resampler library
    Change-Id: Iac73c184e51dc65de29894a6aab280a9afe3eeea

--- Android.mk
+++ Android.mk
@@ -37,3 +37,20 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/include
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES := \
+	libspeex/resample.c
+
+LOCAL_MODULE:= libspeexresampler
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS+= -DEXPORT= -DFIXED_POINT -O3 -fstrict-aliasing -fprefetch-loop-arrays
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include
+
+include $(BUILD_SHARED_LIBRARY)

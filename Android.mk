5c5843bf0f6eac23cdfeaa2d61665f5494dff4cf
Zhiqiang Wang <wang.9264@gmail.com>
2012-07-06 14:39:34 -0400

    add speex encoder,decoder support
    Change-Id: I5280176dcf0f9a08785b4bfb930b8a34d14d5790

--- Android.mk
+++ Android.mk
@@ -20,11 +20,47 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES := \
-	libspeex/mdf.c \
-	libspeex/preprocess.c \
-	libspeex/filterbank.c \
+	libspeex/bits.c \
+	libspeex/buffer.c \
+	libspeex/cb_search.c \
+	libspeex/exc_10_16_table.c \
+	libspeex/exc_10_32_table.c \
+	libspeex/exc_20_32_table.c \
+	libspeex/exc_5_256_table.c \
+	libspeex/exc_5_64_table.c \
+	libspeex/exc_8_128_table.c \
 	libspeex/fftwrap.c \
-	libspeex/smallft.c
+	libspeex/filterbank.c \
+	libspeex/filters.c \
+	libspeex/gain_table.c \
+	libspeex/gain_table_lbr.c \
+	libspeex/hexc_10_32_table.c \
+	libspeex/hexc_table.c \
+	libspeex/high_lsp_tables.c \
+	libspeex/jitter.c \
+	libspeex/kiss_fft.c \
+	libspeex/kiss_fftr.c \
+	libspeex/lpc.c \
+	libspeex/lsp.c \
+	libspeex/lsp_tables_nb.c \
+	libspeex/ltp.c \
+	libspeex/mdf.c \
+	libspeex/modes.c \
+	libspeex/modes_wb.c \
+	libspeex/nb_celp.c \
+	libspeex/preprocess.c \
+	libspeex/quant_lsp.c \
+	libspeex/resample.c \
+	libspeex/sb_celp.c \
+	libspeex/scal.c \
+	libspeex/smallft.c \
+	libspeex/speex.c \
+	libspeex/speex_callbacks.c \
+	libspeex/speex_header.c \
+	libspeex/stereo.c \
+	libspeex/vbr.c \
+	libspeex/vq.c \
+	libspeex/window.c
 
 LOCAL_MODULE:= libspeex
 
